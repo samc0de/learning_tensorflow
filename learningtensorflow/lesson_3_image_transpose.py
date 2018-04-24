24ed893aae13c7237046fb1650c4fe7bc493817b
sam.gofc <smahabole@google.com>
2018-04-24 11:58:44 +0530

    Update to allow passing axis for action.
    Doesn't yet work like that with transpose, the colour int base can become invalid, needs rethinking for this.

--- learningtensorflow/lesson_3_image_transpose.py
+++ learningtensorflow/lesson_3_image_transpose.py
@@ -13,40 +13,49 @@ import argparse
 PATH = '/tmp/MarshOrchid.jpg'
 
 
-def transpose(img_array):
+def transpose(img_array, axis):
   '''Transpose and return input image array.'''
-  transposed = tf.transpose(img_array, perm=[1, 0, 2])
+  # Find a better way for this!!!
+  # Doesn't work!!! Image(x, y, z), z must be a valid colour int base.
+  # perm = [1, 0, 2] if axis < 2 else [2, 1, 0]
+  perm = [1, 0, 2]
+  transposed = tf.transpose(img_array, perm=perm)
   with tf.Session() as sess:
     transposed_img = sess.run(transposed)
   return transposed_img
 
 
-def reverse(img_array):
+def reverse(img_array, axis):
   '''Reverse and return the input image.
 
   Allow passing in axis later.
   '''
   # Same name as funcname! Can't use reversed to avoid masking builtin.
   # reverse = tf.reverse(img_array, dims=[True, False, False])
-  reverse = tf.reverse(img_array, axis=[0])
+  reverse = tf.reverse(img_array, axis=[axis])
   with tf.Session() as sess:
     reverse = sess.run(reverse)
   return reverse
 
 
 if __name__ == '__main__':
+  # Have clearer options, maybe like -r(everse), -t(ranspose).
   parser = argparse.ArgumentParser()
   parser.add_argument(
       '--path', '-p', help='Path of image to be transposed.', default=PATH)
+  # Better add a subcommand.
   parser.add_argument('--action', '-a', choices=('transpose', 'reverse'),
                       default='transpose', help='Action to take on image.')
+  parser.add_argument('--axis', '-x', type=int, choices=[0, 1, 2],
+                      help='Axis to take action along.', default=0)
   arguments = parser.parse_args()
   path = arguments.path
   img_array = image.imread(path)
+  axis = arguments.axis
   if arguments.action == 'reverse':
-    image = reverse(img_array)
+    image = reverse(img_array, axis)
   else:
-    image = transpose(img_array)
+    image = transpose(img_array, axis)
 
   pyplot.imshow(image)
   pyplot.show()
