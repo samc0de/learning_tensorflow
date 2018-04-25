2d468b1eda7271086012fb8d323600cca9b227ee
sam.gofc <smahabole@google.com>
2018-04-25 17:00:58 +0530

    Moved two different actions to subcommands.
    1. Different actions are better as separate subcommands.
    2. Added a note about this being not so clear as now the subcommand actions are same for both and their difference was the main reasons behind the idea of subcommands.

--- learningtensorflow/lesson_3_image_processing.py
+++ learningtensorflow/lesson_3_image_processing.py
@@ -13,19 +13,34 @@ import argparse
 PATH = '/tmp/MarshOrchid.jpg'
 
 
-def transpose(img_array, axis):
-  '''Transpose and return input image array.'''
-  # Find a better way for this!!!
-  # Doesn't work!!! Image(x, y, z), z must be a valid colour int base.
-  # perm = [1, 0, 2] if axis < 2 else [2, 1, 0]
-  perm = [1, 0, 2]
-  transposed = tf.transpose(img_array, perm=perm)
-  with tf.Session() as sess:
-    transposed_img = sess.run(transposed)
-  return transposed_img
+# Defaults overlap with argparse args default system, but this is useful in
+# inter function calls.
+def transpose(img_array, diagonal=False):
+  '''Transpose and return input image array.
+
+  Args:
+    img_array: Array representing the image to transpose.
+    diagonal: Diagonal to transpose along, 0 (default) for primary, 1 for
+      secondary.
+
+  Returns:
+    Array that can be displayed, for showing transpose along the given axis.
+  '''
+  if not diagonal:
+    perm = [1, 0, 2]
+    transposed = tf.transpose(img_array, perm=perm)
+    with tf.Session() as sess:
+      transposed_img = sess.run(transposed)
+    return transposed_img
+
+  # To flip along secondary axis, reverse image, flip along primary and again
+  # reverse.
+  reversed_image = reverse(img_array)
+  reverse_flipped = transpose(reversed_image)
+  return reverse(reverse_flipped)
 
 
-def reverse(img_array, axis):
+def reverse(img_array, axis=0):
   '''Reverse and return the input image.
 
   Allow passing in axis later.
@@ -38,24 +53,42 @@ def reverse(img_array, axis):
   return reverse
 
 
-if __name__ == '__main__':
-  # Have clearer options, maybe like -r(everse), -t(ranspose).
+def main():
   parser = argparse.ArgumentParser()
   parser.add_argument(
       '--path', '-p', help='Path of image to be transposed.', default=PATH)
-  # Better add a subcommand.
-  parser.add_argument('--action', '-a', choices=('transpose', 'reverse'),
-                      default='transpose', help='Action to take on image.')
-  parser.add_argument('--axis', '-x', type=int, choices=[0, 1, 2],
-                      help='Axis to take action along.', default=0)
+  # Better add a subcommand parser.
+  subcommands = parser.add_subparsers(dest='image_action')
+
+  transpose_parser = subcommands.add_parser(
+      'transpose', help='Flip an image along one of its diagonals.')
+  transpose_parser.add_argument('--diagonal', '-d', type=int, choices=[0, 1],
+                      help='Diagonal to transpose along.', default=0)
+  transpose_parser.set_defaults(func=transpose)
+
+  reverse_parser = subcommands.add_parser('reverse', help='Flip an image.')
+  # Better way to map to axis 1 and 0? Make arg -axis 1 with choices(0, 1)
+  reverse_parser.add_argument(
+      '--vertical', '-v', action='store_true',
+      help='Whether to flip vertically.', default=False)
+  reverse_parser.set_defaults(func=reverse)
+
   arguments = parser.parse_args()
   path = arguments.path
   img_array = image.imread(path)
-  axis = arguments.axis
-  if arguments.action == 'reverse':
-    image = reverse(img_array, axis)
+  # image = arguments.func(img_array, arguments)
+  if arguments.image_action == 'reverse':
+    img = reverse(img_array, 0 if arguments.vertical else 1)
   else:
-    image = transpose(img_array, axis)
+    img = transpose(img_array, arguments.diagonal)
 
-  pyplot.imshow(image)
+  pyplot.imshow(img)
   pyplot.show()
+
+
+# The main reasons to move to subcommands was the differring args. Now here we
+# have only (0, 1) in both the cases. Also try moving back the implementation to
+# just --action reverse --x 1.
+
+if __name__ == '__main__':
+  main()
